c504dd0683ac335bfc8acd0c619c0b330f2e91d7
MalikOseni <hosmaad@gmail.com>
2022-11-29 15:34:23 +0100

    finished product

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const adviceSpace = document.getElementById('adviceSlip');
+const adviceID = document.getElementById('adviceId');
 const Btn = document.getElementById('switchBtn');
 const superHero = () => {
     fetch('	https://api.adviceslip.com/advice')
         .then(response => response.json())
         .then(json => {
+            adviceID.innerText=`${json.slip.id}`
         adviceSpace.innerText=`${json.slip.advice}`
             // console.log(json.slip.advice)
             const text = adviceSpace.innerText;
             console.log(`text:${text}`)
             const speech = new SpeechSynthesisUtterance(text);
-            speech.pitch = 2;
+            speech.pitch = 20;
 
             window.speechSynthesis.speak(speech);
     })
